710d49b3d359bbbaca6cabff6d8802a6da9a6604
HermannBjorgvin <hermann3646@gmail.com>
2015-10-05 03:33:42 +0000

    Added method .clean() to API
    Ensures data-type as string and removes non-digit characters

--- kennitala.js
+++ kennitala.js
@@ -2,24 +2,6 @@
 
 	var kennitala = {};
 
-	kennitala.isValidCompany = function(){
-		var kt = formatKennitala(kennitala);
-
-		if (!isCompany(kt)) {
-			return false;
-		};
-
-		var sum =	kt.charAt(0) * 3 + kt.charAt(1) * 2 +
-					kt.charAt(2) * 7 + kt.charAt(3) * 6 +
-					kt.charAt(4) * 5 + kt.charAt(5) * 4 +
-					kt.charAt(6) * 3 + kt.charAt(7) * 2;
-
-		var remainder = 11 - (sum % 11);
-		var secretNr = kt.substr(8, 1);
-
-		return (remainder == 11 && secretNr == 0) || remainder == secretNr;
-	}
-
 	kennitala.isValidPerson = function(kennitala){
 		var kt = formatKennitala(kennitala);
 
@@ -36,31 +18,54 @@
 		var secretNr = kt.substr(8, 1);
 
 		return (remainder == 11 && secretNr == 0) || remainder == secretNr;
-	}
+	};
 
+	kennitala.isValidCompany = function(){
+		var kt = formatKennitala(kennitala);
+
+		if (!isCompany(kt)) {
+			return false;
+		};
+
+		var sum =	kt.charAt(0) * 3 + kt.charAt(1) * 2 +
+					kt.charAt(2) * 7 + kt.charAt(3) * 6 +
+					kt.charAt(4) * 5 + kt.charAt(5) * 4 +
+					kt.charAt(6) * 3 + kt.charAt(7) * 2;
+
+		var remainder = 11 - (sum % 11);
+		var secretNr = kt.substr(8, 1);
+
+		return (remainder == 11 && secretNr == 0) || remainder == secretNr;
+	};
+
+	kennitala.clean = function(kennitala){
+		return formatKennitala(kennitala);
+	};
+
+	// People have first two characters between 1-31
 	function isPerson(kt){
 		var d = kt.substr(0, 2);
 
 		return d > 0 && d <= 31; 
 	};
 
+	// Companies have first two characters between 41-71
 	function isCompany(kt){
 		var d = kt.substr(0, 2);
 
 		return d > 40 && d <= 71;
 	};
 
+	// Ensures datatype is string, then removes all non-digit characters from kennitala
 	function formatKennitala(p_kennitala){
-		// Make sure kennitala is string
 		var kennitala = ""+p_kennitala;
 
-		// Remove unwanted characters from kennitala
 		kennitala = kennitala.replace(/[^0-9]/, '');
 
 		return kennitala;
 	};
 
-	// AMD/CommonJS/PlainJS wrapper
+	// UMD wrapper
 	if(typeof module !== 'undefined' && module.exports) {
 		module.exports = kennitala;
 	} else if (typeof define === 'function' && define.amd){
