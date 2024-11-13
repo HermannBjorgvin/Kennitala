3c653c5fd9a9d775b47ced4c4a9bb5d2ea4454ed
Hermann Björgvin <hermann@hermann.is>
2024-11-13 10:30:12 +0000

    Release/v2.0.2 (#32)
    * v2.0.2

--- dist/types/generation.d.ts
+++
@@ -1,3 +0,0 @@
-declare const generatePerson: (date: Date, startingIncrement?: number) => string | undefined;
-declare const generateCompany: (date: Date) => string | undefined;
-export { generatePerson, generateCompany };
--- dist/types/validation.d.ts
+++
@@ -1,8 +0,0 @@
-import { ValidationOptions } from "./types";
-declare const evaluate: (kt: string, entityEvaluationFn: ((kt: string) => boolean) | null) => boolean;
-declare const isValidDate: (kt: string) => boolean;
-declare const isPerson: (kt: string) => boolean;
-declare const isTestPerson: (kt: string) => boolean;
-declare const isCompany: (kt: string) => boolean;
-declare const getDefaultOptions: (options?: ValidationOptions) => ValidationOptions;
-export { isValidDate, isPerson, isCompany, isTestPerson, evaluate, getDefaultOptions, };
