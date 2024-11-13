885dd466c761902254edc5892846ee0ef2c262bc
Hermann Björgvin Haraldsson <hermann.haraldsson@marel.com>
2024-11-13 10:19:30 +0000

    Fixing issue with v2.0.0 merge

--- dist/types/utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const MAGIC_NUMBERS: number[];
-export declare const padZero: (num: number) => string;
-export declare const sanitizeInput: (kennitala: string) => string | undefined;
-export declare const isTemporary: (kt: string) => boolean;
-export declare const getCentury: (centuryCode: number) => string | null;
-export declare const calculateCheckDigit: (kt: string) => number | null;
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,15 +1,11 @@
-// rollup.config.js
-
 import typescript from "rollup-plugin-typescript2";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
 
 export default [
-  // ESM build
   {
     input: "src/index.ts",
     output: {
-      dir: "dist/esm",
+      dir: "dist",
       format: "es",
       sourcemap: true,
     },
@@ -22,28 +18,6 @@ export default [
         browser: true,
         preferBuiltins: false,
       }),
-    ],
-  },
-  // CJS build
-  {
-    input: "src/index.ts",
-    output: {
-      name: "kennitala",
-      dir: "dist/umd",
-      format: "umd",
-      sourcemap: true,
-      exports: "named",
-    },
-    plugins: [
-      typescript({
-        tsconfig: "./tsconfig.json",
-        useTsconfigDeclarationDir: true,
-      }),
-      nodeResolve({
-        browser: true,
-        preferBuiltins: false,
-      }),
-      commonjs(),
     ],
   },
 ];
