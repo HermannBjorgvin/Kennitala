210a52afee07b39cb94b8f00c66cfb1ba2010007
Hermann Björgvin Haraldsson <hermann.haraldsson@marel.com>
2024-11-13 08:47:52 +0000

    Updated node version in actions. Updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,37 +1,9 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+// @ts-check
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default [{
-    ignores: ["**/dist/", "**/node_modules/"],
-}, ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"), {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
-
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "module",
-
-        parserOptions: {
-            project: ["./tsconfig.json"],
-        },
-    },
-
-    rules: {
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-    },
-}];
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended
+);
